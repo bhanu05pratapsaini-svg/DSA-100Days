39a00b905f8e970d3b66fa6d18b4d5298405ec65
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2026-04-14 22:30:46 +0530

    Day70

---
+++ code70.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define INF INT_MAX
+
+struct Edge {
+    int u, v, w;
+};
+
+void bellmanFord(int n, int m, struct Edge edges[], int src) {
+    int dist[n];
+
+    for (int i = 0; i < n; i++)
+        dist[i] = INF;
+
+    dist[src] = 0;
+
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+
+            if (dist[u] != INF && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+
+        if (dist[u] != INF && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE\n");
+            return;
+        }
+    }
+
+    printf("Shortest distances from source %d:\n", src);
+    for (int i = 0; i < n; i++) {
+        if (dist[i] == INF)
+            printf("%d -> INF\n", i);
+        else
+            printf("%d -> %d\n", i, dist[i]);
+    }
+}
+
+int main() {
+    int n = 5; 
+    int m = 8; 
+
+    struct Edge edges[] = {
+        {0, 1, -1},
+        {0, 2, 4},
+        {1, 2, 3},
+        {1, 3, 2},
+        {1, 4, 2},
+        {3, 2, 5},
+        {3, 1, 1},
+        {4, 3, -3}
+    };
+
+    int src = 0;
+
+    bellmanFord(n, m, edges, src);
+
+    return 0;
+}
